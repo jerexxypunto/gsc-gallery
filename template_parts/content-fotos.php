9511fb66c9a1d3c8a16f11f45ae1b1cad33d68b3
Jeremias <jerexxypunto@gmail.com>
2021-11-09 15:25:53 -0300

    baner de entradas fotos botones listos

--- template_parts/content-fotos.php
+++ template_parts/content-fotos.php
@@ -12,72 +12,87 @@
             </h2>
         </div> 
         <div class="tm-gallery-fotos-container">
-            <div class="nowrap row tm-mb-90 tm-gallery">
-                <?php if($fotos->have_posts()): ?>
-                    <?php while($fotos->have_posts()): $fotos->the_post(); ?>
-                    <div class="col-4 mb-5 card-picture">
-                        <figure class="effect-ming tm-video-item photo-cover">
-                            <?php if(has_post_thumbnail()){
-                                the_post_thumbnail('medium_large');
-                            } ?>
-                            <figcaption class="d-flex align-items-center justify-content-center">
-                                <h2><?php the_title() ?></h2>
-                                <a href="<?php the_permalink(); ?>">View more</a>
-                            </figcaption>                    
-                        </figure>
-                        <div class="d-flex justify-content-between tm-text-gray">
-                            <span class="tm-text-gray-light"><?php the_date();?></span>
-                            <span><?php the_author(); ?></span>
+            <button class="boton-izquierda">
+                <span class="fas fa-chevron-left"></span>
+            </button>
+            <div class="nowrap-tm-galery-container">
+                <div class="nowrap row tm-gallery">
+                    <?php if($fotos->have_posts()): ?>
+                        <?php while($fotos->have_posts()): $fotos->the_post(); ?>
+                        <div class="col-4 card-picture">
+                            <figure class="effect-ming tm-video-item photo-cover">
+                                <?php if(has_post_thumbnail()){
+                                    the_post_thumbnail('medium_large');
+                                } ?>
+                                <figcaption class="d-flex align-items-center justify-content-center">
+                                    <h2><?php the_title() ?></h2>
+                                    <a href="<?php the_permalink(); ?>">View more</a>
+                                </figcaption>                    
+                            </figure>
+                            <div class="d-flex justify-content-between tm-text-gray">
+                                <span class="tm-text-gray-light"><?php the_date();?></span>
+                                <span><?php the_author(); ?></span>
+                            </div>
                         </div>
-                    </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            </div> <!-- row -->
-            <div id="fotos" class="arrow-button-container">
-                <button class="boton-izquierda">
-                    <span class="fas fa-chevron-left"></span>
-                </button>
-                <button class="boton-derecha">
-                    <span class="fas fa-chevron-right"></span>
-                </button>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div> <!-- row -->
             </div>
+            <button class="boton-derecha">
+                <span class="fas fa-chevron-right"></span>
+            </button>
         </div>
         <script>
-            const botonDerecha = document.querySelector("#fotos .boton-derecha"); 
-            const botonIzquierda = document.querySelector("#fotos .boton-izquierda"); 
-            const itemsEntries = document.querySelectorAll(".tm-gallery-fotos-container .nowrap .card-picture");
+            const botonDerecha = document.querySelector(".boton-derecha"); 
+            const botonIzquierda = document.querySelector(".boton-izquierda"); 
+            const itemsEntriesContainer = document.querySelector(".nowrap.row.tm-gallery");
 
-            let currentValue;
-            let now;
+            let iterationNumber;
+            let currentIteration = 0;
+            let itemsHijos;
+            let currentWidth;
+
+            let quatityDisplacement = 0;
 
             botonDerecha.addEventListener("click", (e) => deslizar(e));
-            botonIzquierda.addEventListener("click", (e) => deslizar(e));
+            botonIzquierda.addEventListener("click", (e) => deslizar(e));              
 
+            function changeItem(direccion){
+                itemsHijos = itemsEntriesContainer.children;
+                iterationNumber = Math.trunc(itemsHijos.length / 3);
+                currentWidth = itemsEntriesContainer.clientWidth;
+                if(direccion === "fa-chevron-right"){
+                    if(currentIteration < iterationNumber){
+                        currentIteration++;
+                        quatityDisplacement = quatityDisplacement + currentWidth;
+                    }else{
+                        currentIteration = 0;
+                        quatityDisplacement = 0;
+                    }
+                }else if(direccion === "fa-chevron-left"){
+                    if(currentIteration > 0){
+                        currentIteration--;
+                        if(Math.sign(quatityDisplacement) === -1){
+                            quatityDisplacement = quatityDisplacement + currentWidth;
+                        }else if(Math.sign(quatityDisplacement) === 1){
+                            quatityDisplacement = quatityDisplacement - currentWidth;
+                        }
+                    }else{
+                        currentIteration = iterationNumber;
+                        quatityDisplacement = currentWidth * -1;
+                    }
+                }
+            }
 
-             let getPadding = (txt)=>{
-                    let pL = window.getComputedStyle(txt, null).getPropertyValue('padding-left');
-                    return pL;
-            }              
 
             function deslizar(direccion) {
                 let classElement = direccion.target.classList[1];
-                let currentWidth = itemsEntries[0].clientWidth;
-                let iterationNumber = Math.trunc(itemsEntries.length / 3) ;
-                let paddingTotal = getPadding(itemsEntries[0]).split("px");
-                paddingTotal = parseInt(paddingTotal[0]);
-                now = (((currentWidth + paddingTotal) * 3) * iterationNumber);
-                currentValue = 0; 
-                if (classElement == "fa-chevron-right"){
-                    currentValue === now ? currentValue = 0 : currentValue = currentValue + now;
-                    itemsEntries.forEach(element => {
-                        element.style.transform = `translateX(-${currentValue}px)`;
-                    });
-                }else if (classElement == "fa-chevron-left"){
-                    currentValue === 0 ? currentValue = now : currentValue = currentValue - now; 
-                    itemsEntries.forEach(element => {
-                        element.style.transform = `translateX(${currentValue}px)`;
-                    });
+                changeItem(classElement)
+                if (classElement === "fa-chevron-right") {
+                    itemsEntriesContainer.style.transform = `translateX(-${quatityDisplacement}px)`;
+                }else if (classElement === "fa-chevron-left"){
+                    itemsEntriesContainer.style.transform = `translateX(${quatityDisplacement}px)`;
                 }
-                console.log(`currentValue:${currentValue} y now:${now}`);
+                
             }
         </script>
