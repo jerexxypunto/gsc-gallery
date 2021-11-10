8b5881a6132b3793a5de40a9a122427d0e997e9f
Jeremias <jerexxypunto@gmail.com>
2021-11-10 09:22:06 -0300

    estilos modificados para adaptar a mobile

--- template_parts/content-entries.php
+++ template_parts/content-entries.php
@@ -50,32 +50,34 @@
 	return null;
 }
 ?>
-         <div class="row mb-4">
-            <h2 class="col-6 tm-text-primary">
-                Últimas entradas
-            </h2>
+         <div class="container">
+             <div class="row mb-4">
+                <h2 class="col-6 tm-text-primary">
+                    Últimas entradas
+                </h2>
         </div> 
-        <div class="row tm-mb-90 tm-gallery">
-            <?php if(have_posts()): ?>
-                <?php while(have_posts()):the_post(); ?>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5 card-picture">
-                    <figure class="effect-ming tm-video-item photo-cover">
-                        <?php if(has_post_thumbnail()){
-                            the_post_thumbnail('medium_large');
-                        } ?>
-                        <figcaption class="d-flex align-items-center justify-content-center">
-                            <h2><?php the_title() ?></h2>
-                            <a href="<?php the_permalink(); ?>">View more</a>
-                        </figcaption>                    
-                    </figure>
-                    <div class="d-flex justify-content-between tm-text-gray">
-                        <span class="tm-text-gray-light"><?php the_date();?></span>
-                        <span><?php the_author(); ?></span>
+             <div class="row tm-mb-90 tm-gallery">
+                <?php if(have_posts()): ?>
+                    <?php while(have_posts()):the_post(); ?>
+                    <div class="col-4 mb-5 card-picture">
+                        <figure class="effect-ming tm-video-item photo-cover">
+                            <?php if(has_post_thumbnail()){
+                                the_post_thumbnail('medium_large');
+                            } ?>
+                            <figcaption class="d-flex align-items-center justify-content-center">
+                                <h2><?php the_title() ?></h2>
+                                <a href="<?php the_permalink(); ?>">View more</a>
+                            </figcaption>                    
+                        </figure>
+                        <div class="d-flex justify-content-between tm-text-gray">
+                            <span class="tm-text-gray-light"><?php the_date();?></span>
+                            <span><?php the_author(); ?></span>
+                        </div>
                     </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>     
+                <div class="col-12">
+                    <?php echo bootstrap_pagination(); ?> 
                 </div>
-                <?php endwhile; ?>
-            <?php endif; ?>     
-            <div class="col-12">
-                <?php echo bootstrap_pagination(); ?> 
-            </div>
-        </div> <!-- row -->
+            </div> <!-- row -->
+         </div>
--- template_parts/content-fotos.php
+++ template_parts/content-fotos.php
@@ -6,41 +6,43 @@
   $fotos = new WP_Query($args);
 ?>
         
-        <div class="row mb-4">
-            <h2 class="col-6 tm-text-primary">
-                Últimas fotos
-            </h2>
-        </div> 
-        <div class="tm-gallery-fotos-container">
-            <button class="boton-izquierda">
-                <span class="fas fa-chevron-left"></span>
-            </button>
-            <div class="nowrap-tm-galery-container">
-                <div class="nowrap row tm-gallery">
-                    <?php if($fotos->have_posts()): ?>
-                        <?php while($fotos->have_posts()): $fotos->the_post(); ?>
-                        <div class="col-4 card-picture">
-                            <figure class="effect-ming tm-video-item photo-cover">
-                                <?php if(has_post_thumbnail()){
-                                    the_post_thumbnail('medium_large');
-                                } ?>
-                                <figcaption class="d-flex align-items-center justify-content-center">
-                                    <h2><?php the_title() ?></h2>
-                                    <a href="<?php the_permalink(); ?>">View more</a>
-                                </figcaption>                    
-                            </figure>
-                            <div class="d-flex justify-content-between tm-text-gray">
-                                <span class="tm-text-gray-light"><?php the_date();?></span>
-                                <span><?php the_author(); ?></span>
+        <div class="container">
+            <div class="row mb-4">
+                <h2 class="col-6 tm-text-primary">
+                    Últimas fotos
+                </h2>
+            </div> 
+            <div class="tm-gallery-fotos-container">
+                <button class="boton-izquierda boton-fotos">
+                    <span class="fas fa-chevron-left"></span>
+                </button>
+                <div class="nowrap-tm-galery-container">
+                    <div class="nowrap row tm-gallery">
+                        <?php if($fotos->have_posts()): ?>
+                            <?php while($fotos->have_posts()): $fotos->the_post(); ?>
+                            <div class="col-4 card-picture">
+                                <figure class="effect-ming tm-video-item photo-cover">
+                                    <?php if(has_post_thumbnail()){
+                                        the_post_thumbnail('medium_large');
+                                    } ?>
+                                    <figcaption class="d-flex align-items-center justify-content-center">
+                                        <h2><?php the_title() ?></h2>
+                                        <a href="<?php the_permalink(); ?>">View more</a>
+                                    </figcaption>                    
+                                </figure>
+                                <div class="d-flex justify-content-between tm-text-gray">
+                                    <span class="tm-text-gray-light"><?php the_date();?></span>
+                                    <span><?php the_author(); ?></span>
+                                </div>
                             </div>
-                        </div>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div> <!-- row -->
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div> <!-- row -->
+                </div>
+                <button class="boton-derecha boton-fotos">
+                    <span class="fas fa-chevron-right"></span>
+                </button>
             </div>
-            <button class="boton-derecha">
-                <span class="fas fa-chevron-right"></span>
-            </button>
         </div>
         <script>
             const botonDerecha = document.querySelector(".boton-derecha"); 
