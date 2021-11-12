0efa1677f30e5aef45dbbeae46ebd7e6b34d4de6
Jeremias <jerexxypunto@gmail.com>
2021-11-12 09:36:27 -0300

    configuracion para el reproductor de video

--- single-video.php
+++ single-video.php
@@ -1,4 +1,7 @@
 <?php get_header() ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets-video-player/css/video-js.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets-video-player/css/estilos.css">
+    <script src="<?php echo get_template_directory_uri()?>/assets-video-player/js/video.js"></script>
     <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="<?php echo get_template_directory_uri()?>/img/hero.jpg">
         <form class="d-flex tm-search-form">
             <input class="form-control tm-search-input" type="search" placeholder="Search" aria-label="Search">
@@ -11,9 +14,40 @@
     <?php
       if(have_posts()):
         while (have_posts()): the_post(); ?>
-             <article class="tm-text-primary pt-5 pb-3">
-               <h1 class="tm-text-primary"><?php the_title(); ?></h1>
-             </article>
+             <div class="contenedor">
+              <!-- <video class="gsc-video video-js vjs-16-9 vjs-big-play-centered" data-setup="{}" controls id="fm-video">
+                <source src="./content/video/MatrimonioMyM.mp4" type="video/mp4">
+              </video> -->
+              <article class="py-3">
+                <h1 class="tm-text-primary pb-4 pt-3"><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+              </article>
+            </div>
+            <script>
+              let videoWp = document.querySelector("video");
+              const clases = ["gsc-video", "video-js", "vjs-16-9", "vjs-big-play-centered"];
+              const id = "gsc-video";
+
+              const source = document.createElement("SOURCE");
+              const currentURL = videoWp.getAttribute("src");
+              source.setAttribute('src', currentURL);
+
+              videoWp.setAttribute('id', id);
+              videoWp.setAttribute('data-setup', {});
+              videoWp.appendChild(source);
+              videoWp.setAttribute('src','false');
+
+              clases.forEach(clase => {
+                  videoWp.classList.add(clase);
+              });
+
+              videoWp
+
+              var reproductor = videojs('gsc-video', {
+                fluid: true
+              });
+
+            </script>
               <?php  
                 endwhile;
               endif;
